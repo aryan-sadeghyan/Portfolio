550e68a4b38bb160d37574a515aead7fde40538e
aryan <aryansadeghyan54@gmail.com>
2024-06-01 14:42:08 +0330

    tweaks 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import "../font.css?inline";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import { AnimatePresence, motion } from "framer-motion";
+
 import HeroTypeWriter from "./HeroTypeWriter";
 
 export default function home() {
   return (
     <div name='home' className='bg-[#0a192f] w-full h-screen'>
       <div className=' h-full max-w-[1000px] mx-auto  px-[50px] flex flex-col justify-center '>
-        <p className='text-pink-600 text-xl'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h2 className='text-[#bec5d9] text-xl'>Hi, my name is</h2>
+        <h1 className=' text-4xl sm:text-7xl font-bold text-[#bec5d9]'>
           Aryan Sadeghyan
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] '>
+        <h2 className=' text-4xl sm:text-7xl font-bold text-[#bec5d9] '>
           And I'm{" "}
           <HeroTypeWriter
             speed={100}
@@ -20,7 +20,7 @@ export default function home() {
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           Im a full-stack developer specializing in building and occasionally
-          designing) exceptional digital experiences. currentyly, Im focused on
+          designing exceptional digital experiences. currentyly, Im focused on
           building responsive full-stack web applications.
         </p>
         <button className='w-[140px] text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
